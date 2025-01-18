9c99cecd261d744264ce8445afed036079c7b649
CEE <archmagece@users.noreply.github.com>
2025-01-18 12:23:00 +0900

    kkaao

--- lib/omniauth/strategies/kakao.rb
+++ lib/omniauth/strategies/kakao.rb
@@ -1,72 +1,97 @@
 # frozen_string_literal: true
 
-require "jwt"
-require "oauth2"
-require "omniauth/strategies/oauth2"
-require "uri"
+require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
     class Kakao < OmniAuth::Strategies::OAuth2
-      # ALLOWED_ISSUERS = ["kauth.kakao.com", "https://kauth.kakao.com"].freeze
-      # BASE_SCOPE_URL = 'https://kauth.kakao.com/oauth/authorize/'
-      # BASE_SCOPES = %w[profile account_email openid].freeze
-      # DEFAULT_SCOPE = "account_email,profile"
-      # IMAGE_SIZE_REGEXP = /(s\d+(-c)?)|(w\d+-h\d+(-c)?)|(w\d+(-c)?)|(h\d+(-c)?)|c/
-      # AUTHORIZE_OPTIONS = %i[access_type hd login_hint prompt request_visible_actions scope state redirect_uri include_granted_scopes enable_granular_consent openid_realm device_id device_name]
+      DEFAULT_SCOPE = "account_email,profile".freeze
 
-      BASE_URL = "https://kauth.kakao.com"
+      BASE_URL = "https://kauth.kakao.com".freeze
+      AUTHORIZE_URL = "/oauth/authorize".freeze
+      AUTHORIZE_TOKEN_URL = "/oauth/token".freeze
 
-      AUTHORIZE_URL = "/oauth/authorize"
-      TOKEN_URL = "/oauth/token"
-      TOKEN_INFO_URL = "/oauth/tokeninfo"
-      # https://kapi.kakao.com/v2/user/me
-      USER_INFO_URL = '/v1/oidc/userinfo'
-      OPENID_CONFIG_URL = "/.well-known/openid-configuration"
+      TOKEN_INFO_URL = "/oauth/tokeninfo".freeze
 
-      option :name, "kakao"
+      OPENID_CONFIG_URL = "https://kauth.kakao.com/.well-known/openid-configuration".freeze
+
+      # USER_INFO_URL = 'v1/oidc/userinfo'
+      USER_INFO_URL = "https://kapi.kakao.com/v2/user/me".freeze
+
       option :client_options,
              site: BASE_URL,
              authorize_url: AUTHORIZE_URL,
-             token_url: TOKEN_URL
+             token_url: AUTHORIZE_TOKEN_URL
 
-      option :authorize_options, [:scope]
-
-      uid { raw_info["sub"].to_s }
+      uid { raw_info["id"].to_s }
 
       info do
-        {
-          name: raw_info["name"],
-          # username: raw_info["username"],
-          nickname: raw_info["nickname"],
-          image: raw_info["picture"],
-          email: raw_info["email"],
-          email_verified: raw_info["email_verified"],
-          gender: raw_info["gender"],
-          birthday: raw_info["birthday"],
-          phone_number: raw_info["phone_number"],
-          phone_number_verified: raw_info["phone_number_verified"],
+        hash = {
+          name: raw_info["properties"]["nickname"],
+          username: raw_info["kakao_account"]["email"],
+          image: raw_info["properties"]["thumbnail_image"]
         }
+        if raw_info["kakao_account"]["has_email"] && raw_info["kakao_account"]["is_email_verified"] && raw_info["kakao_account"]["is_email_valid"]
+          hash[:email] = raw_info["kakao_account"]["email"]
+        end
+        raw_info
       end
 
       extra do
         { raw_info: raw_info }
       end
 
-      def raw_info
-        @raw_info ||= access_token.get(USER_INFO_URL).parsed
+      def callback_url
+        options.redirect_url || (full_host + callback_path)
       end
 
-      # dev
-      # def callback_url
-      #   # full_host = 'http://127.0.0.1:4200'
-      #   full_host = 'http://localhost:4200'
-      #   full_host + script_name + callback_path
+      # def authorize_params
+      #   options.authorize_params[:state] = SecureRandom.hex(24)
+      #
+      #   if OmniAuth.config.test_mode
+      #     @env ||= {}
+      #     @env["rack.session"] ||= {}
+      #   end
+      #
+      #   params = options.authorize_params
+      #                   .merge(options_for("authorize"))
+      #                   .merge(pkce_authorize_params)
+      #
+      #   params[:client_id] = options.client_id  # client_id 추가
+      #
+      #   session["omniauth.pkce.verifier"] = options.pkce_verifier if options.pkce
+      #   session["omniauth.state"] = params[:state]
+      #
+      #   params
       # end
-      def callback_url
-        full_host = 'http://localhost:4200'
-      #   full_host + script_name + callback_path
-        options.redirect_url || (full_host + callback_path)
+
+      def auth_token_params
+        verifier = session.delete("omniauth.pkce.verifier")
+        params = {
+          code: request.params["code"],
+          client_id: options.client_id,
+          client_secret: options.client_secret,
+          redirect_uri: callback_url,
+          grant_type: "authorization_code"
+        }
+        params[:code_verifier] = verifier if verifier
+        params
+      end
+
+      def build_access_token
+        verifier = request.params["code"]
+        token = client.auth_code.get_token(verifier, {
+          redirect_uri: callback_url,
+          client_id: options.client_id,
+          # client_secret: options.client_secret
+        }.merge(token_params.to_hash(symbolize_keys: true)), deep_symbolize(options.auth_token_params))
+        token
+      end
+
+      private
+
+      def raw_info
+        @raw_info ||= access_token.get(USER_INFO_URL).parsed
       end
     end
   end
