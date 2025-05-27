02f20a72131cac9e635ac4945b9329abe62bd659
CEE <archmagece@users.noreply.github.com>
2025-05-27 16:47:26 +0900

    verup dep

--- plugin.rb
+++ plugin.rb
@@ -9,7 +9,7 @@
 
 enabled_site_setting :enable_login_with_kakao
 
-gem "sb-omniauth-kakao", "0.3.4"
+gem "sb-omniauth-kakao", "0.3.5"
 # gem 'sb-omniauth-kakao', git: 'git@github.com:ScriptonBasestar/sb-omniauth-kakao.git'
 
 # register_asset "stylesheets/discourse-custom-oauth-icons.scss"
